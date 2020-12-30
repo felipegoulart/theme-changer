b0aac667984df3e8bcdd87edd7308ab6571c8ad1
Felipe <luisfelipegoulart94@gmail.com>
2020-12-30 00:05:15 -0300

    Refatored theme objects for best understand

--- app.js
+++ app.js
@@ -3,62 +3,56 @@ const switchTheme = document.querySelector('#switch')
 const iconTheme = document.querySelector('.iconTheme')
 
 const lightThemeColors = {
-  '--background-color': '#e2e2ee',
-  '--primary-color': '#d4d4e7',
-  '--secundary-color': '#dddde9',
-  
-  '--primary-text-color': '#484855',
-  '--secundary-text-color': '#24242c',
-  'title': 'lightTheme'
+  title: 'lightTheme',
+  colors: {
+    '--background-color': '#e2e2ee',
+    '--primary-color': '#d4d4e7',
+    '--secundary-color': '#dddde9',
+    
+    '--primary-text-color': '#484855',
+    '--secundary-text-color': '#24242c',
+  },
+  icon: './img/sun.svg'
 }
 
 const darkThemeColors = {
-  '--background-color': '#202027',
-  '--primary-color': '#17171d',
-  '--secundary-color': '#292933',
-  
-  '--primary-text-color': '#dedeeb',
-  '--secundary-text-color': '#b1b1c4',
-  'title': 'darkTheme'
+  title: 'darkTheme',
+  colors: {
+    '--background-color': '#202027',
+    '--primary-color': '#17171d',
+    '--secundary-color': '#292933',
+    
+    '--primary-text-color': '#dedeeb',
+    '--secundary-text-color': '#b1b1c4',
+  },
+  icon: './img/moon.svg'
 }
 
-const lightThemeIcon = './img/sun.svg'
-const darkThemeIcon = './img/moon.svg'
+const changeStyle = (theme) => {
+  const { colors, icon } = theme
 
-const changeStyle = (theme, icon) => {
-  Object.keys(theme).map(key => html.style.setProperty(key, theme[key]))
+  Object.keys(colors).map(key => html.style.setProperty(key, colors[key]))
   iconTheme.style.backgroundImage = `url(${icon})`
 
-  setThemeInLocalStorage(theme, icon)
+  setThemeInLocalStorage(theme)
 }
 
-const setThemeInLocalStorage = (theme, icon) => {
-  const themeColorsConverted = JSON.stringify(theme)
+const setThemeInLocalStorage = (theme) => localStorage.setItem('theme', JSON.stringify(theme))
+const getThemeInLocalStorage = () => JSON.parse(localStorage.getItem('theme'));
 
-  localStorage.setItem('colorsTheme', themeColorsConverted)
-  localStorage.setItem('iconTheme', icon)
-}
-
-const getThemeInLocalStorage = () => {
-  const colorsThemeParsed = JSON.parse(localStorage.getItem('colorsTheme'))
-  const icon = localStorage.getItem('iconTheme')
-
-  return [colorsThemeParsed, icon]
-}
-
-const changeThemeIfChecked = ({ target }) => {
-  target.checked ? changeStyle(darkThemeColors, darkThemeIcon) : changeStyle(lightThemeColors, lightThemeIcon)
-}
+const changeThemeIfChecked = ({ target }) => target.checked ? changeStyle(darkThemeColors)
+  : changeStyle(lightThemeColors)
 
 const themeInLocalStorage = getThemeInLocalStorage()
 
-const checkWhichThemeIsSavedInLocalStorage = (theme) => {
-  if(theme?.title === 'darkTheme') switchTheme.checked = true
+const checkWhichThemeIsSavedInLocalStorage = ({ title }) => {
+  if(title === 'darkTheme') switchTheme.checked = true
   else switchTheme.checked = false
 }
 
-checkWhichThemeIsSavedInLocalStorage(...themeInLocalStorage)
+checkWhichThemeIsSavedInLocalStorage(themeInLocalStorage)
 
-if (localStorage.key('theme')) changeStyle(...themeInLocalStorage)
+const existThemeInLocalStorage = localStorage.key('theme')
+if (existThemeInLocalStorage) changeStyle(themeInLocalStorage)
 
 switchTheme.addEventListener('change', changeThemeIfChecked)
